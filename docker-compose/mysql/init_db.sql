5b7b419d4b7c935138a9a0af9d2e7c52d57c0cfc
chwlr <ch.williamr@gmail.com>
2023-04-18 00:08:52 +0800

    initial commit

---
+++ docker-compose/mysql/init_db.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `students`;
+
+CREATE TABLE `students` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(20) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `phone_number` varchar(12) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB
