1d73e4562d729ce0e6531e2aebbe41a1a7447419
chwlr <ch.williamr@gmail.com>
2023-04-18 14:15:16 +0800

    update init_db.sql and README.md

--- docker-compose/mysql/init_db.sql
+++ docker-compose/mysql/init_db.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `students`;
 
 CREATE TABLE `students` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `code` varchar(20) NOT NULL,
+  `uuid` varchar(255) NOT NULL,
   `full_name` varchar(255) NOT NULL,
   `phone_number` varchar(12) NOT NULL,
   `city` varchar(20) NOT NULL,
